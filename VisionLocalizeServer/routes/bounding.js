06d9a3b0ef157d3ec2a915f012fe034a9277477a
Cole Gleason <cgleason@cs.cmu.edu>
2016-04-18 13:07:24 -0400

    fix bug in bounding results

--- VisionLocalizeServer/routes/bounding.js
+++ VisionLocalizeServer/routes/bounding.js
@@ -35,7 +35,7 @@ var MAX_USER_HISTORY_LENGTH = 10;
  * 	map : ID of map
  *  image : binary image data
  *  bounding: list of points to create bounding boxes on the image.
- *    should be x1_top,y1_top,x1_bottom,y1_bottom,x2_top,y2_top ...
+ *    should be x1_top,y1_top,x1_bottom,y1_bottom,x2_top,y2_top .,
  *  cx : center to restrict localization area (optional)
  *  cy : center to restrict localization area (optional)
  *  cz : center to restrict localization area (optional)
@@ -59,11 +59,40 @@ exports.estimatePost = function(req, res) {
     	console.log('Error : Map ID is not specified');
         return sendErrorResponse(404, 'Map ID is not specified');
     }
+    if (!req.body.bounding) {
+    	console.log('Error : Bounding boxes not specified');
+        return sendErrorResponse(404, 'Bouding boxes not specified');
+    }
     if (!req.files || !req.files.image) {
     	console.log('Error : Image data is not specified');
         return sendErrorResponse(404, "Image data is not specified");
     }
 
+    // check bounding parameter
+    if (!Array.isArray(req.body.bounding) || req.body.bounding.length <= 0) {
+    	console.log('Error : Bounding boxes not specified');
+        return sendErrorResponse(404, 'Bouding boxes not specified');
+    }
+    var boundingBoxError = false;
+    var boundingBoxes = [];
+    req.body.bounding.forEach(function(boundingBox) {
+        if (!Array.isArray(boundingBox) || boundingBox.length != 4) {
+            boundingBoxError = true;
+        }
+        boundingBoxes = boundingBoxes.concat(boundingBox);
+    });
+    if (boundingBoxError) {
+    	console.log('Error : Bounding boxes have incorrect format');
+        return sendErrorResponse(404, 'Bouding boxes have incorrect format');
+    }
+    boundingBoxes = boundingBoxes.map(function(n) {
+        return parseInt(n, 10);
+    });
+    if (Math.min(boundingBoxes) < 0) {
+        console.log('Error : Bounding points cannot be negative');
+        return sendErrorResponse(404, 'Bouding boxes points cannot be negative');
+    }
+    console.log(boundingBoxes);
     // check user parameter
     if (!share.userNameMap[req.body.user]) {
     	console.log('Error : User ID is not valid');
@@ -77,9 +106,9 @@ exports.estimatePost = function(req, res) {
     	console.log('Error : Map ID is not valid');
     	return sendErrorResponse(500, 'Map ID is not valid');
     }
-	var sfmDataDir = share.mapNameMap[req.body.map]['sfm_data_dir'];
-	var matchDir = share.mapNameMap[req.body.map]['match_dir'];
-	var aMatFile = share.mapNameMap[req.body.map]['a_mat_file'];
+    var sfmDataDir = share.mapNameMap[req.body.map]['sfm_data_dir'];
+    var matchDir = share.mapNameMap[req.body.map]['match_dir'];
+    var aMatFile = share.mapNameMap[req.body.map]['a_mat_file'];
     var scaleImage = share.userNameMap[req.body.user]['scale_image'];
 
     async.waterfall([ function(callback) {
@@ -91,47 +120,38 @@ exports.estimatePost = function(req, res) {
                 res.end();
             } else {
                 var result;
-                if (req.body.beacon) {
-                    if (req.body.cx && req.body.cy && req.body.cz && req.body.radius) {
-                        result = localizeImage.localizeImageBufferBeacon(req.body.user, kMatFile, distMatFile, scaleImage,
-                        		req.body.map, sfmDataDir, matchDir, aMatFile,
-                        		data, req.body.beacon, [req.body.cx, req.body.cy, req.body.cz], req.body.radius);
-                    } else {
-                        result = localizeImage.localizeImageBufferBeacon(req.body.user, kMatFile, distMatFile, scaleImage,
-                        		req.body.map, sfmDataDir, matchDir, aMatFile, data, req.body.beacon);
-                    }
-                } else {
-	                if (req.body.cx && req.body.cy && req.body.cz && req.body.radius) {
-	                    result = localizeImage.localizeImageBuffer(req.body.user, kMatFile, distMatFile, scaleImage,
-	                    		req.body.map, sfmDataDir, matchDir, aMatFile,
-	                    		data, [req.body.cx, req.body.cy, req.body.cz], req.body.radius);
-	                } else {
-	                    result = localizeImage.localizeImageBuffer(req.body.user, kMatFile, distMatFile, scaleImage,
-	                    		req.body.map, sfmDataDir, matchDir, aMatFile, data);
-	                }
-                }
-
-                console.log('localization result : ' + result);
-                var jsonObj;
-                if (result && result.length>0) {
-                	jsonObj = {'estimate':{'t':result.slice(0,3), 'R':[result.slice(3,6),result.slice(6,9),result.slice(9,12)]}};
-
-                    // update users' history
-                	if (!share.userHistories[req.body.user]) {
-                		share.userHistories[req.body.user] = [];
-                	}
-                	if (share.userHistories[req.body.user].length>MAX_USER_HISTORY_LENGTH) {
-                		share.userHistories[req.body.user].shift();
-                	}
-                	share.userHistories[req.body.user].push(jsonObj);
-                } else {
-                    jsonObj = {'estimate':{'t':[], 'R':[]}};
-                }
-               	// Send back the result as a JSON
-                callback(null, JSON.stringify(jsonObj));
+                result = localizeImage.getBoundedFeatures(req.body.user, kMatFile, distMatFile, scaleImage,
+                                                          req.body.map, sfmDataDir, matchDir, aMatFile, data, boundingBoxes);
             }
+            var jsonObj;
+            if (result && result.length>0) {
+                jsonObj = {'boundingBoxResults':[]};
+                var boundingBoxPointNums = result.splice(0, req.body.bounding.length);
+                boundingBoxPointNums.forEach(function (n) {
+                    var points = result.splice(0, n*3);
+                    var reshapedPoints = [];
+                    for (var i =0; i < points.length; i = i + 3) {
+                        reshapedPoints.push(
+                            {x: points[i], y: points[i+1], z: points[i+2]}
+                        );
+                    }
+                    jsonObj['boundingBoxResults'].push(reshapedPoints);
+                });
+                // update users' history
+                if (!share.userHistories[req.body.user]) {
+                    share.userHistories[req.body.user] = [];
+                }
+                if (share.userHistories[req.body.user].length>MAX_USER_HISTORY_LENGTH) {
+                    share.userHistories[req.body.user].shift();
+                }
+                share.userHistories[req.body.user].push(jsonObj);
+            } else {
+                jsonObj = [];
+            }
+            // Send back the result as a JSON
+            callback(null, JSON.stringify(jsonObj));
         });
-    } ], function(err, result) {
+    }], function(err, result) {
         if (err) {
             sendErrorResponse(500, err.message);
         }
